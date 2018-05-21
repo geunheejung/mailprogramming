48216f9c9d4a5bb17327692cf65eac5d3d518407
junggeunhee <gh.jung1@albamapp.com>
2018-05-21 17:09:10 +0900

    string 타입으로 풀었던 문제 int형으로 변환

--- 4.js
+++ 4.js
@@ -5,7 +5,33 @@ const f = ([...texts]) => {
   const beforeTexts = JSON.stringify(texts.slice(0, textsLen / 2)),
         afterTexts = JSON.stringify(texts.slice((textsLen / 2) + 1, textsLen).reverse());
 
+
+
   return beforeTexts === afterTexts;
 }
 
-console.log(f('12421'));
+const f2 = (n) => {
+  if(n < 0 || (n % 10 == 0 && n != 0)) {
+    return false;
+  }
+
+  let revertedHalf = n % 10,
+      count = 10;
+
+  while (n > revertedHalf) {
+    revertedHalf = revertedHalf * 10 + (Math.floor(n / count) % 10)
+    count *= 10;
+  }
+  console.log(revertedHalf)
+
+  return revertedHalf === n;
+}
+
+
+console.log(f2(212));
+
+
+
+
+
+
